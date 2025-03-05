161119f59d39533e2511305d4e00ca88ee6ebead
hhirsch <hhirsch@uni-osnabrueck.de>
2025-03-05 10:15:01 +0100

    mit25

--- Ex03_DEBfitting_Folsomia.py
+++ Ex03_DEBfitting_Folsomia.py
@@ -43,7 +43,8 @@ def load_data():
     Einlesen der Kontrolldaten für Folsomia.
     """
 
-    data = pd.read_csv('folsomia_temperature_cadmium_growth_tidy.csv', header = 5)
+    data = pd.read_csv('folsomia_temperature_cadmium_growth_tidy25.csv', header = 5)
+    #data = pd.read_csv('folsomia_temperature_cadmium_growth_tidy.csv', header = 5)
     #data = data[data['T_cels']==20]
     data = data.loc[lambda df : df.C_F == 0]
     data = data.assign(S = collembola_length_to_weight(data.length_mm)) 
@@ -193,7 +194,7 @@ def setup_modelfit():
     
     f.intguess = { 
         'Idot_max_rel' : f.defaultparams.spc['Idot_max_rel'],#Einflussreichster Parameter
-        'eta_AS_0' : f.defaultparams.spc['eta_AS_0'],#Wachstumsparameter
+        #'eta_AS_0' : f.defaultparams.spc['eta_AS_0'],#Wachstumsparameter
         'k_M_0' : f.defaultparams.spc['k_M_0'],
         'T_A' : f.defaultparams.spc['T_A']
 
@@ -226,8 +227,10 @@ def fit_model():
 
     fig, ax = plot_data(f.data)
 
-    sns.lineplot(sim_opt, x = 't_day', y = 'S', hue='T_cels')
+    sns.lineplot(sim_opt, x = 't_day', y = 'S',
+                 hue='T_cels', palette = ['forestgreen', 'mediumblue', 'darkviolet'])
 
-    ax.legend()
-
+    ax.set_title("Einfluss der Temperatur auf Struktur von Folsomia")
+    ax.legend(title='Temperatur [°C]')
+    
     return f
